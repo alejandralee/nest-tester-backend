ff4589372b8ae414047cae0b223718bdebd30297
Alejandra <alejandra.lee@gmail.com>
2025-06-13 13:38:34 +0200

    add current time endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/current-time')
+  getCurrentTime(): string {
+    return this.appService.getCurrentTime();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,10 @@ export class AppService {
   getHello(): string {
     return 'Hello World! This is me testing a deploy!';
   }
+
+  getCurrentTime(): string {
+    // get the current time in UTC and format it as a string
+    const now = new Date();
+    return now.toUTCString();
+  }
 }
