49fd4e7be45952d3dd559f153f163566f460222f
Alejandra <alejandra.lee@gmail.com>
2025-06-18 13:21:06 +0200

    rename db var

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,11 +4,11 @@ import { User } from './entity/User';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DATASOURCE_HOST || '', // use env variable or fallback
+  host: process.env.DATASOURCE_HOST || 'localhost', // use env variable or fallback
   port: 5432,
   username: process.env.DATASOURCE_USERNAME || '',
   password: process.env.DATASOURCE_PASSWORD || '',
-  database: process.env.DATASOURCE_DATABASE || 'nest-tester-db',
+  database: process.env.DATASOURCE_DB || 'nest-tester-db',
   synchronize: true,
   logging: false,
   entities: [User],
